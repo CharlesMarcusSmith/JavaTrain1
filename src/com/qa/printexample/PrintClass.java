070e3b47bc1aa31883db3dae5832d1733c5c6ec6
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-14 13:02:22 +0100

    CS: have learned more about data types and truncation of floats

--- src/com/qa/printexample/PrintClass.java
+++ src/com/qa/printexample/PrintClass.java
@@ -6,8 +6,15 @@ public class PrintClass {
 //		System.out.println(result);
 //		Is the same as:
 		System.out.println(resultString("Original Value"));					//we don't need to store the value, but we typically do because its easier to read.
+		
+		
+		System.out.println(floatExample());
 	}
 	public static String resultString(String myMessage) {
 		return myMessage + " I've added something to this!";
+	}
+	
+	public static float floatExample() {
+		return  7f / 2f;
 	}
 }
