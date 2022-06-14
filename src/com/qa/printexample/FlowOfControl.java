bb08c0cf7a34efa233108e20462e138776afef2a
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-14 16:42:03 +0100

    Work into scope and flow of control, but have hit an error on static

--- src/com/qa/printexample/FlowOfControl.java
+++ src/com/qa/printexample/FlowOfControl.java
@@ -1,5 +1,33 @@
 package com.qa.printexample;
 
 public class FlowOfControl {
-
+String globalVar = "This is known as Class scope, or a class variable";	
+	public static void main(String[] args) {
+		methodOne();
+		System.out.println(methodTwo());
+		
+		System.out.println("Line 3");
+		testScope2();
+		globalPrinter();
+		
+	}
+	public static void methodOne() {
+	System.out.println("Line 1");
+	}
+	public static String methodTwo() {
+		return "Line 2";
+	}
+	public static String testScope1() {
+		String testVar = "Loco";
+		return testVar;
+	}
+	public static void testScope2() {
+		String testThief = testScope1();
+		System.out.println("We can only take the value " + testThief + " from the method, not the variable itself");
+	}
+	public void globalPrinter() {
+		System.out.println("We can however print global variables," + globalVar);
+	}
+	
+	
 }
