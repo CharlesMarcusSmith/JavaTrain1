0794385c585c3861237a3d7a4bc9f5eddede47da
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-14 14:22:54 +0100

    CS: have now tested boolean returns and use of booleans as variables

--- src/com/qa/printexample/PrintClass.java
+++ src/com/qa/printexample/PrintClass.java
@@ -9,12 +9,23 @@ public class PrintClass {
 		
 		
 		System.out.println(floatExample());
+		System.out.println(compareNumbers());
 	}
 	public static String resultString(String myMessage) {
 		return myMessage + " I've added something to this!";
 	}
 	
 	public static float floatExample() {
-		return  7f / 2f;
+		return  7f / 2f;													//we have to put f after the numbers, because Java automatically truncates int division (rounds it)
+																			//so even 7 / 2, returned then saved as a float, will equal 3.0
+																			//we have to make the numbers floats before we divide them therefore.
 	}
+	public static boolean compareNumbers() {								//the following code compares two numbers then returns a boolean
+		boolean equal = false;
+		if (2 == 2) {
+			equal = true;
+		}
+		return equal; 
+	}
+	
 }
