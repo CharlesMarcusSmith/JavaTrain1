3b437a71abd19124b6e3f6b0fbbf4235229d0a3a
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-14 11:07:05 +0100

    trying to create the file

---
+++ src/com/qa/printexample/PrintClass.java
@@ -0,0 +1,11 @@
+package com.qa.printexample;
+
+public class PrintClass {
+	public static void main(String[] args) {
+		printMessage();
+	}
+	public static void printMessage() {
+		String myMessage = "I had random keyboard driver death";
+		System.out.println(myMessage);
+	}
+}
