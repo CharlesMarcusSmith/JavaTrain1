acc00e3d12e8bca00712f012d93261d6e69f85e7
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-15 13:35:33 +0100

    CS: completed Calculator exercise in Operators, QA community

--- src/com/qa/calculatorexercise/Calculator.java
+++ src/com/qa/calculatorexercise/Calculator.java
@@ -3,6 +3,12 @@ package com.qa.calculatorexercise;
 public class Calculator {
 	int numA;
 	int numB;
+	String name; 
+	double phyR; 
+	double chemR;
+	double bioR;
+	double total;
+	
 	
 	public void Calculations(int numA, int numB){
 		this.numA = numA;
@@ -12,6 +18,32 @@ public class Calculator {
 		double numA1 = numA;
 		double numB1 = numB;
 		System.out.println("Multiplication of " + numA1 + " and " + numB1 + " equals " + (numA1/numB1));
+		System.out.println("");
+	}
+	
+	
+	public void ResultsReceived(String name, double phyR, double chemR, double bioR) {
+		this.name = name;
+		this.phyR = phyR;
+		this.chemR = chemR;
+		this.bioR = bioR;
+		this.total = phyR + chemR + bioR;
+		
+		System.out.println(name + "'s results are as follows:");
+		System.out.println("Physics:" + phyR + "/150.");
+		System.out.println("Chemistry:" + chemR + "/150.");
+		System.out.println("Biology:" + bioR + "/150.");
+		System.out.println("Total:" + total);
+		System.out.println("");
+	}
+	
+	public void ResultsPercentage() {
+		System.out.println(name + "'s result percentages are as follows:");
+		System.out.println("Physics:" + Math.round((phyR/150)*100));
+		System.out.println("Chemistry:" + Math.round((chemR/150)*100));
+		System.out.println("Biology:" + Math.round((bioR/150)*100));
+		System.out.println("Total:" + Math.round((total/450)*100));
+		System.out.println();
 	}
 	
 }
--- src/com/qa/calculatorexercise/Runner.java
+++ src/com/qa/calculatorexercise/Runner.java
@@ -5,6 +5,10 @@ public class Runner {
 		Calculator ca = new Calculator();
 		ca.Calculations(5,3);
 		
+		Calculator s1 = new Calculator();
+		s1.ResultsReceived("Charles", 75, 64, 25);
+		s1.ResultsPercentage();
 	}
+	
 	
 }
