e063ce12a4b1c9b5f85e365bf34857ef1d33a0c9
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-15 12:40:51 +0100

    CS: passing in the numbers into the basic maths method, to make it a more resuable method, as appose to limiting ourself to class specific numbers

--- src/com/qa/operator/Operators.java
+++ src/com/qa/operator/Operators.java
@@ -1,15 +1,16 @@
 package com.qa.operator;
 
 public class Operators {
-	public int num1 = 5;
-	public int num2 = 3;
-	
-	public void BasicMaths() {
+	public int num1;
+	public int num2;
+	public void BasicMaths(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
 		System.out.println(num1 + num2);
 		System.out.println(num1 - num2);
 		System.out.println(num1 * num2);
 		System.out.println(num1 / num2);
 		System.out.println("Modulus is %, so the line above is division,");
-		System.out.println("but modulus of the same is:" + num1 % num2);
+		System.out.println("but modulus of the same is:" + num1 % num2);			//modulus is the remainder of a division
 	}
 }
