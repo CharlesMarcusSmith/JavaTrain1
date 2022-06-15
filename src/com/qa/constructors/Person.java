a1a005b0db7b530b06db985d6cb0280eb4229f90
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-15 12:21:27 +0100

    CS: have learned static and non-static under 'constructors' package, and now am moving onto Operators

--- src/com/qa/constructors/Person.java
+++ src/com/qa/constructors/Person.java
@@ -1,14 +1,17 @@
 package com.qa.constructors;
 
 public class Person {
-	public String name;
+	public String name;														//Non-static, only exsists in the object itself.
 	public int age;
 	public int shoeSize = 10;												//if we call line 15, this will be the default size.
+	public static int numOfPeople = 0;										//static variable = exsists in the class not the object.
 	
 	public Person(String inName, int inAge, int inShoeSize) {				//usually these are the same name as the constructor variable as they don't conflict
 		this.name = inName;													// i'm using inName etc to make reflection easier in future if needed
 		this.age = inAge;
 		this.shoeSize = inShoeSize;
+		System.out.println("Person created");
+		numOfPeople ++;
 	}
 	
 	//below we are going to demonstrate overloading, which is the same method as Person() above using
