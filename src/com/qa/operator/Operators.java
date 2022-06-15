f6a8a47045352e06acce4bedbccd95756bb5ef2f
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-15 12:27:51 +0100

    Operators non-static explored

--- src/com/qa/operator/Operators.java
+++ src/com/qa/operator/Operators.java
@@ -1,5 +1,13 @@
 package com.qa.operator;
 
 public class Operators {
-
+	public int num1 = 5;
+	public int num2 = 3;
+	
+	public void BasicMaths() {
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+	}
 }
--- src/com/qa/operator/Runner.java
+++ src/com/qa/operator/Runner.java
@@ -1,5 +1,8 @@
 package com.qa.operator;
 
 public class Runner {
-
+	public static void main(String[] args) {
+		Operators op = new Operators();			//we can only access Basic Maths through an object as it isnt't static, so we create 'op'.
+		op.BasicMaths(); 						//calls that object's method
+	}
 }
