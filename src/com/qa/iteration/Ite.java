9496d14bd4805c238ea8571a3461a9ce941f5381
llamaladejam <charliemarcussmith@googlemail.com>
2022-06-16 16:28:53 +0100

    CS: have completed the 2 flowchart iteration exercises.

---
+++ src/com/qa/iteration/Ite.java
@@ -0,0 +1,21 @@
+package com.qa.iteration;
+
+public class Ite {
+		
+	public void iteOne() {
+		
+		for(int A = 100; A <= 200; A++) {
+			System.out.println("A");
+		}
+	}
+	
+	public void iteTwo() {
+		int A = 100;
+			if(A%2 == 0) {
+				System.out.println("-");
+			} else {
+				System.out.println("*");
+			}
+			A++;
+		}	
+}
---
+++ src/com/qa/iteration/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.iteration;
+
+public class Runner {
+	public static void main(String[] args) {
+		//Iteration exercvise 1 is an flowchart image, which asks us to print "A" when a variable called A is not greater than 200, A begins at 1 in flowchart.
+		Ite iteA = new Ite();
+		iteA.iteOne();
+		
+		Ite iteB = new Ite();
+		iteB.iteTwo();
+		
+	}
+}
